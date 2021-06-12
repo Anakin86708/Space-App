5518bbe9d73f010e9502d3fce7f18f8b1397a94d
Ariel Tadeu da Silva <silva.ariel2013@gmail.com>
2021-06-12 12:22:32 -0300

    Exibindo nome dos astronautas

--- lib/views/astronauts_page/gridCard.dart
+++ lib/views/astronauts_page/gridCard.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:space_app/model/api/astronautData.dart';
 import 'package:space_app/model/astronautData.dart';
+import 'package:space_app/theme/appColors.dart';
 
 class AstronautGridCard extends StatefulWidget {
   static const cardPadding = const EdgeInsets.all(8.0);
@@ -11,16 +12,19 @@ class AstronautGridCard extends StatefulWidget {
       {String title = 'Title',
       String content = 'Content',
       String imageUrl = '',
-      bool isFavorited = false, this.apiData}) {
+      bool isFavorited = false,
+      this.apiData}) {
     data =
         LocalAstronautData(title, content, imageUrl, isFavorited: isFavorited);
   }
 
   factory AstronautGridCard.fromAstronautData(AstronautData data) {
     return AstronautGridCard(
-        title: data.name,
-        content: data.bio,
-        imageUrl: data.profileImageThumbnail, apiData: data,);
+      title: data.name,
+      content: data.bio,
+      imageUrl: data.profileImageThumbnail,
+      apiData: data,
+    );
   }
 
   @override
@@ -38,7 +42,13 @@ class _AstronautGridCardState extends State<AstronautGridCard> {
           child: Padding(
             padding: AstronautGridCard.cardPadding,
             child: Stack(
-              children: [widget.data.buildImage()],
+              children: [
+                Center(child: widget.data.buildImage()),
+                Container(
+                  alignment: Alignment.bottomCenter,
+                  child: Text(widget.data.title,style: TextStyle(color: AppColors.accent, backgroundColor: AppColors.primary.shade100),),
+                )
+              ],
             ),
           ),
         ));
