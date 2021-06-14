ec200a024f636a450e3743011a29ad01e4ae6e39
Leonardo <leeo_skw@hotmail.com>
2021-06-13 21:01:29 -0300

    Adição de informações na pagína do astronauta.

--- lib/views/astronauts_page/astrounatsPage.dart
+++ lib/views/astronauts_page/astrounatsPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:space_app/model/api/astronautData.dart';
+import 'package:space_app/theme/themeData.dart';
 
 class AstronautsPage extends StatelessWidget {
   const AstronautsPage(this.data) : super();
@@ -12,7 +13,108 @@ class AstronautsPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(data.name),
       ),
-      body: Text("Teste"),
+      body: post(),
+    );
+  }
+
+  Widget post() {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Stack(
+            children: [
+              banner(),
+              Container(
+                child: Align(
+                  child: Padding(
+                    padding: const EdgeInsets.all(13.0),
+                  ),
+                  alignment: Alignment.topRight,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            width: double.infinity,
+            child: Padding(
+              padding: const EdgeInsets.all(15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  _generateTitle(),
+                  SizedBox(height: 20),
+                  _generateContentText(),
+                  SizedBox(height: 20),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+        ],
+      ),
+    );
+  }
+
+  Widget banner() {
+    return Container(
+      constraints: BoxConstraints.expand(
+        height: 200.0,
+      ),
+      child: generateImage(),
+    );
+  }
+
+  Image generateImage() {
+    return Image.network(
+      data.profileImage,
+      fit: BoxFit
+          .cover, // fills the image as much as it can within its container.
+    );
+  }
+
+  Text _generateTitle() {
+    return Text(
+      data.name,
+      style: AppTheme.postStyle["titleStyle"],
+    );
+  }
+
+  Column _generateContentText() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        _generateAstronautStatus(),
+        _generateAstronautAgency(),
+        SizedBox(height: 10),
+        _generateAstronautBio(),
+      ],
+    );
+  }
+
+  Text _generateAstronautBio() {
+    return Text(
+      data.bio,
+      style: AppTheme.postStyle["contentStyle"],
+      textAlign: AppTheme.postStyle["contentJustify"],
+    );
+  }
+
+  Text _generateAstronautStatus() {
+    List<String> splittedDateDeath = data.dateDeath.split("-");
+    var dateDeath = data.dateDeath != ''
+        ? 'Status: ' + data.status + ' (${splittedDateDeath[0]})'
+        : 'Status: ' + data.status;
+
+    return Text(
+      dateDeath,
+      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+    );
+  }
+
+  Text _generateAstronautAgency() {
+    return Text(
+      'Agency: ' + data.agency,
+      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
     );
   }
 }
